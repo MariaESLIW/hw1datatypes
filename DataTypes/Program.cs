1f89cf5aca73745143d0fbaf17b2518fd8bc8e3e
MariaESLIW <121664848+MariaESLIW@users.noreply.github.com>
2022-12-30 11:20:37 +0200

    solution

---
+++ DataTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, values!");
+Console.Write("byte values min:" + byte.MinValue + " max:");
+Console.WriteLine(byte.MaxValue);
+Console.Write("int values min:" + int.MinValue + " max:");
+Console.WriteLine(int.MaxValue);
+Console.Write("long values min:" + long.MinValue + " max:");
+Console.WriteLine(long.MaxValue);
+Console.Write("short value min:" + short.MinValue + " max:");
+Console.WriteLine(short.MaxValue);
+Console.Write("float values min:" + float.MinValue + " max:");
+Console.WriteLine(float.MaxValue);
+Console.Write("double values min:" + double.MinValue + " max:");
+Console.WriteLine(double.MaxValue);
+Console.Write("char values min:" + char.MinValue + " max:");
+Console.WriteLine(char.MaxValue);
